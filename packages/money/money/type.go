315e795018f2339a3be1eceba097ebdfb8023f78
Gustavo Ocanto <gustavoocanto@gmail.com>
2026-04-20 14:01:39 +0800

    format

---
+++ packages/money/money/type.go
@@ -0,0 +1,15 @@
+package money
+
+import (
+	"github.com/bedrock/packages/money/calculator"
+	"github.com/bedrock/packages/money/currency"
+)
+
+// Amount is a data structure that stores the amount being used for calculations.
+type Amount = calculator.Amount
+
+// Money represents a monetary value with an amount and a currency.
+type Money struct {
+	amount   Amount             `db:"amount"`
+	currency *currency.Currency `db:"currency"`
+}
